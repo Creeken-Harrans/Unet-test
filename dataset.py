4fcafc4dfe296701cf8c1d9b79d2ce2e72bfa909
Creeken <18853932590@163.com>
2026-02-24 20:48:57 +0800

    Unet-Learning

--- dataset.py
+++ dataset.py
@@ -14,7 +14,9 @@ class ISBI_Loader(Dataset):
         search_pattern = os.path.join(data_path, "image/*.png")
         print("正在搜索的路径:", search_pattern)
         # 执行搜索
-        self.imgs_path = glob.glob(search_pattern)
+        self.imgs_path = glob.glob(
+            search_pattern
+        )  # 返回一个列表，包含所有匹配的文件路径
         print("找到的图片列表:", self.imgs_path)
         print("找到的图片数量:", len(self.imgs_path))
 
@@ -39,13 +41,15 @@ class ISBI_Loader(Dataset):
             raise ValueError(f"无法读取标签: {label_path}")
 
         # 将数据转为单通道的图片
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # 这里出来的像素值是0-255的整数
         label = cv2.cvtColor(label, cv2.COLOR_BGR2GRAY)
         image = image.reshape(1, image.shape[0], image.shape[1])
         label = label.reshape(1, label.shape[0], label.shape[1])
         # 处理标签，将像素值为255的改为1
         if label.max() > 1:
-            label = label / 255
+            label = (
+                label / 255
+            )  # 为什么只处理标签, 因为标签的像素值是0和255，代表两类，而模型输出的是0和1，所以需要将标签的像素值也转换为0和1。训练图片的像素值是0-255的整数，代表灰度值，不需要转换。
         # 随机进行数据增强，为2时不做处理
         flipCode = random.choice([-1, 0, 1, 2])
         if flipCode != 2:
